6bdfc9a6d43265a92cbbc04673f2accb79563fb1
정재희 <wogml5890@gmail.com>
2024-05-10 17:32:51 +0900

    first commit

---
+++ src/main/java/com/ssafy/MohaengApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohaengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohaengApplication.class, args);
+	}
+
+}
